fc2d685aadd08e53ad539cdf5386865a06a7ded0
ANanth <ananthvsblab@gmail.com>
2024-11-06 17:39:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import BusRoute from './BusRoute';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <BusRoute/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
